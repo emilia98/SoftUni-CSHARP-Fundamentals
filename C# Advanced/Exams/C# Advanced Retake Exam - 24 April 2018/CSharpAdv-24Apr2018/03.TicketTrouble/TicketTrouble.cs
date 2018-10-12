d654b3a074dad831897073723bb9187214de1945
emilia98 <emilia_nedialkova@abv.bg>
2018-10-13 00:22:28 +0300

    started a new exam (50%)

---
+++ C# Advanced/Exams/C# Advanced Retake Exam - 24 April 2018/CSharpAdv-24Apr2018/03.TicketTrouble/TicketTrouble.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _03.TicketTrouble
+{
+    class TicketTrouble
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            var locationDetails = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string countryCode = locationDetails[0];
+            string cityCode = locationDetails[1];
+
+            var pattern = new Regex(@"(?<curly>\{[^\{\}]*?(\[(?<dest>[A-Z]{3}\s*[A-Z]{2})\])[^\{\}]*?(\[(?<seats>[A-Z]{1}\s*[0-9]{1,2})\])[^\{\}]*?\})|(?<square>\[[^\[\]]*?(\{(?<dest2>[A-Z]{3}\s*[A-Z]{2})\})[^\[\]]*?(\{(?<seats2>[A-Z]{1}\s*[0-9]{1,2})\})[^\[\]]*?\])");
+
+            MatchCollection matches = pattern.Matches(text);
+            var seatsData = new Dictionary<int, List<string>>();
+            var seats = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                var groups = match.Groups;
+                string destination = null;
+                string seat = null;
+
+
+                if (groups["curly"].Success)
+                {
+                    destination = groups["dest"].Value;
+                    seat = groups["seats"].Value;
+                }
+                else if (groups["square"].Success)
+                {
+                    destination = groups["dest2"].Value;
+                    seat = groups["seats2"].Value;
+                }
+
+                var tokens_dest = destination.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string toCountry = tokens_dest[0];
+                string toCity = tokens_dest[1];
+
+                int seat_row = int.Parse(seat.Substring(1));
+                char seat_col = seat[0];
+
+                if (toCountry == countryCode && toCity == cityCode)
+                {
+                    seats.Add(seat);
+                    if (seatsData.ContainsKey(seat_row) == false)
+                    {
+                        seatsData.Add(seat_row, new List<string>());
+                    }
+
+                    seatsData[seat_row].Add(seat);
+                }
+            }
+            if (seats.Count == 2)
+            {
+                Console.WriteLine($"You are traveling to {countryCode} {cityCode} on seats {seats[0]} and {seats[1]}.");
+                return;
+            }
+
+            foreach (var seatsRow in seatsData)
+            {
+                var seatsOnThisRow = seatsRow.Value;
+                int seatsCount = seatsOnThisRow.Count;
+
+                if (seatsCount >= 2)
+                {
+                    Console.WriteLine($"You are traveling to {countryCode} {cityCode} on seats {seatsOnThisRow[0]} and {seatsOnThisRow[1]}.");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exams/C# Advanced Retake Exam - 24 April 2018/CSharpAdv-24Apr2018/04.MovieTime/MovieTime.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _04.MovieTime
+{
+    class MovieTime
+    {
+        static void Main()
+        {
+            string favouriteGenre = Console.ReadLine();
+            string duration = Console.ReadLine();
+            var moviesData = new Dictionary<string, Dictionary<string, TimeSpan>>();
+
+            string input = Console.ReadLine();
+            double totalTime = 0;
+
+            while (input != "POPCORN!")
+            {
+                var tokens = Regex.Split(input, @"\s*\|\s*");
+                string title = tokens[0];
+                string genre = tokens[1];
+
+                var time = Regex.Split(tokens[2], @"\s*\:\s*")
+                                .Select(int.Parse).ToArray();
+                var timespan = new TimeSpan(time[0], time[1], time[2]);
+
+                totalTime += timespan.TotalSeconds;
+
+                if (moviesData.ContainsKey(genre) == false)
+                {
+                    moviesData.Add(genre, new Dictionary<string, TimeSpan>());
+                }
+
+                if (moviesData[genre].ContainsKey(title) == false)
+                {
+                    moviesData[genre].Add(title, new TimeSpan());
+                }
+
+                moviesData[genre][title] = timespan;
+
+                input = Console.ReadLine();
+            }
+
+            var matchingGenre = moviesData[favouriteGenre];
+            var sortedMovies = matchingGenre;
+
+            if (duration == "Short")
+            {
+                sortedMovies = matchingGenre.OrderBy(x => x.Value.TotalSeconds)
+                                            .ThenBy(x => x.Key)
+                                            .ToDictionary(x => x.Key, x => x.Value);
+            }
+            else if (duration == "Long")
+            {
+                sortedMovies = matchingGenre.OrderByDescending(x => x.Value.TotalSeconds)
+                                            .ThenBy(x => x.Key)
+                                            .ToDictionary(x => x.Key, x => x.Value);
+            }
+
+            string chosenMovie = null;
+            string answer;
+
+            foreach (var movie in sortedMovies)
+            {
+                string title = movie.Key;
+                Console.WriteLine(title);
+
+                answer = Console.ReadLine();
+
+                if (answer == "Yes")
+                {
+                    chosenMovie = title;
+                    break;
+                }
+            }
+
+            var totalTimeSpan = TimeSpan.FromSeconds(totalTime);
+            Console.WriteLine($"We're watching {chosenMovie} - {sortedMovies[chosenMovie].ToString()}");
+            Console.WriteLine($"Total Playlist Duration: {totalTimeSpan}");
+        }
+    }
+}
