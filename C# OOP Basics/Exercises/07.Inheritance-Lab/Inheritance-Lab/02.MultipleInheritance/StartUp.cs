cf1aa15f2ced5344e66c2caffcecce4d8d18213d
emilia98 <emilia_nedialkova@abv.bg>
2018-11-01 12:03:43 +0200

    added the LAB

---
+++ C# OOP Basics/Exercises/07.Inheritance-Lab/Inheritance-Lab/02.MultipleInheritance/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            var puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
